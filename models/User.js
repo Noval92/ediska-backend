cf0f73dd807ab12b0d8de8c4d05b97a0968422b5
Noval92 <nurimansyanoval@gmail.com>
2025-05-22 14:38:13 +0700

    users1

--- models/User.js
+++ models/User.js
@@ -1,37 +1,16 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  nama: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  nim: {
-    type: String
-  },
-  fakultas: {
-    type: String
-  },
-  foto: {
-    type: String
-  },
-  // TAMBAHKAN INI:
-  nohp: { type: String, default: '' },
-  domisili: { type: String, default: '' },
-  semester: { type: String, default: '' }, // atau Number, bebas
-});
+  nama: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  nim: { type: String },
+  fakultas: { type: String },
+  foto: { type: String },
+  nohp: { type: String },
+  domisili: { type: String },
+  semester: { type: String },
+}, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
